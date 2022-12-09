dca26c1edf498b61a6acba168eb8f500cd5af95b
ADEDEWE BOLUWATIFE <lilblessingjnr@gmail.com>
2022-12-09 11:20:46 +0100

    First change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("bacl");
+  res.send("My Api Running");
 });
 
 app.listen(3000, () => console.log("listening at 3000"));
